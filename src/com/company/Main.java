b884ff768c9839aad2918c562b930c4e768addd6
Maksim Kalabukhov <bfmaximmm1992@gmail.com>
2021-02-26 11:51:19 +0300

    урок 12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,7 +22,7 @@ public class Main {
         for (int i = 0; i < SIZE; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        System.out.println(System.currentTimeMillis() - a + " первый " + arr[SIZE-1]);
+        System.out.println(System.currentTimeMillis() - a + " первый");
     }
 
     private static void m2(){
@@ -34,7 +34,7 @@ public class Main {
         }
 
         long b = System.currentTimeMillis();
-        
+
         System.arraycopy(arr, 0, a1, 0, HALF);
         System.arraycopy(arr, HALF, a2, 0, HALF);
 
@@ -53,7 +53,7 @@ public class Main {
             @Override
             public void run() {
                 for (int i = 0; i < HALF; i++) {
-                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                    a2[i] = (float)(a2[i] * Math.sin(0.2f + (HALF+i) / 5) * Math.cos(0.2f + (HALF+i) / 5) * Math.cos(0.4f + (HALF+i) / 2));
                 }
                 result++;
                 resultArr(result, a1, a2, arr,b);
@@ -68,7 +68,7 @@ public class Main {
         if (result == 2) {
             System.arraycopy(a1, 0, arr, 0, HALF);
             System.arraycopy(a2, 0, arr, HALF, HALF);
-            System.out.println(System.currentTimeMillis() - b + " второй " + arr[SIZE-1]);
+            System.out.println(System.currentTimeMillis() - b + " второй");
         }
     }
 }
